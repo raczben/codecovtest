9786fcb079e7ba32737f1238f22ca1577fce80cc
Benedek Racz <betontalpfa@gmail.com>
2019-05-25 20:20:24 +0200

    pull ghdl docker

--- test/test_core.py
+++ test/test_core.py
@@ -3,6 +3,7 @@ Test util functions
 """
 import pytest
 import os
+import subprocess
 
 import cct.main
 
@@ -12,3 +13,11 @@ repo_root = os.path.abspath(os.path.join(here, os.pardir))
 
 def test_main():
     assert cct.main.ret42() == 42
+    
+    
+def test_docker():    
+    cmd = 'docker pull ghdl/ghdl'
+    
+    # Pytest asserts the non-zero return status of subprocess
+    out = subprocess.check_output(cmd.split(' '))
+    
